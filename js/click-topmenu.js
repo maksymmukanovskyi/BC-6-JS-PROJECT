fac2b0f243bb41a498a0bec9055e07208900b232
Danyl Krasnoiarov <dkrasnoyarov25@gmail.com>
2018-07-18 16:44:51 +0300

    add album page

---
+++ js/click-topmenu.js
@@ -0,0 +1,14 @@
+// 'use strict';
+// const contentArtist = document.querySelector('#artist');
+// const contentAlbum = document.querySelector('#albums');
+// const content = document.querySelector('.content');
+// const chart = document.querySelectorAll('.chart');
+// const album = document.querySelector('.content__album');
+// // const contentSongs = document.querySelector('#songs');
+//
+// function showAlbum () {
+//     // console.log(event.target);
+//     content.classList.toggle("content__album");
+// }
+//
+// contentAlbum.addEventListener('click', showAlbum);
