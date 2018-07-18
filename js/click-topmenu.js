3dec00207880e62a467501b483f0daecf46879d5
Danyl Krasnoiarov <dkrasnoyarov25@gmail.com>
2018-07-18 16:45:45 +0300

    change album page

--- js/click-topmenu.js
+++ js/click-topmenu.js
@@ -1,14 +1,17 @@
-// 'use strict';
-// const contentArtist = document.querySelector('#artist');
-// const contentAlbum = document.querySelector('#albums');
-// const content = document.querySelector('.content');
-// const chart = document.querySelectorAll('.chart');
-// const album = document.querySelector('.content__album');
-// // const contentSongs = document.querySelector('#songs');
-//
-// function showAlbum () {
-//     // console.log(event.target);
-//     content.classList.toggle("content__album");
-// }
-//
-// contentAlbum.addEventListener('click', showAlbum);
+'use strict';
+const contentArtist = document.querySelector('#artist');
+const contentAlbum = document.querySelector('#albums');
+const content = document.querySelector('.content');
+const chart = document.querySelectorAll('.chart-list');
+const album = document.querySelector('.content__album');
+const divChart = document.querySelector('.chart');
+// const contentSongs = document.querySelector('#songs');
+
+function showAlbum (event) {
+    // console.log(event.target);
+    album.classList.toggle('none');
+    content.classList.toggle('none');
+}
+
+
+divChart.addEventListener('click', showAlbum);
