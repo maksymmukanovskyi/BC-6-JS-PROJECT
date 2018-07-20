e43bbaa2ebeebb138f89df2da98a00a1b3f7f497
Sergey Plaski <splaski@gmail.com>
2018-07-20 15:35:08 +0300

    added templates

---
+++ js/album.js
@@ -0,0 +1,267 @@
+'use strict';
+
+const data = {
+    "album": {
+        "name": "Believe",
+        "artist": "Cher",
+        "mbid": "63b3a8ca-26f2-4e2b-b867-647a6ec2bebd",
+        "url": "https://www.last.fm/music/Cher/Believe",
+        "image": [
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/34s/3b54885952161aaea4ce2965b2db1638.png",
+                "size": "small"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/64s/3b54885952161aaea4ce2965b2db1638.png",
+                "size": "medium"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/174s/3b54885952161aaea4ce2965b2db1638.png",
+                "size": "large"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3b54885952161aaea4ce2965b2db1638.png",
+                "size": "extralarge"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3b54885952161aaea4ce2965b2db1638.png",
+                "size": "mega"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3b54885952161aaea4ce2965b2db1638.png",
+                "size": ""
+            }
+        ],
+        "listeners": "343864",
+        "playcount": "2096965",
+        "tracks": {
+            "track": [
+                {
+                    "name": "Believe",
+                    "url": "https://www.last.fm/music/Cher/_/Believe",
+                    "duration": "240",
+                    "@attr": {
+                        "rank": "1"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "The Power",
+                    "url": "https://www.last.fm/music/Cher/_/The+Power",
+                    "duration": "236",
+                    "@attr": {
+                        "rank": "2"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Runaway",
+                    "url": "https://www.last.fm/music/Cher/_/Runaway",
+                    "duration": "286",
+                    "@attr": {
+                        "rank": "3"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "All or Nothing",
+                    "url": "https://www.last.fm/music/Cher/_/All+or+Nothing",
+                    "duration": "238",
+                    "@attr": {
+                        "rank": "4"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Strong Enough",
+                    "url": "https://www.last.fm/music/Cher/_/Strong+Enough",
+                    "duration": "224",
+                    "@attr": {
+                        "rank": "5"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Dov'è L'amore",
+                    "url": "https://www.last.fm/music/Cher/_/Dov%27%C3%A8+L%27amore",
+                    "duration": "258",
+                    "@attr": {
+                        "rank": "6"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Takin' Back My Heart",
+                    "url": "https://www.last.fm/music/Cher/_/Takin%27+Back+My+Heart",
+                    "duration": "272",
+                    "@attr": {
+                        "rank": "7"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Taxi Taxi",
+                    "url": "https://www.last.fm/music/Cher/_/Taxi+Taxi",
+                    "duration": "304",
+                    "@attr": {
+                        "rank": "8"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "Love Is the Groove",
+                    "url": "https://www.last.fm/music/Cher/_/Love+Is+the+Groove",
+                    "duration": "271",
+                    "@attr": {
+                        "rank": "9"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                },
+                {
+                    "name": "We All Sleep Alone",
+                    "url": "https://www.last.fm/music/Cher/_/We+All+Sleep+Alone",
+                    "duration": "234",
+                    "@attr": {
+                        "rank": "10"
+                    },
+                    "streamable": {
+                        "#text": "0",
+                        "fulltrack": "0"
+                    },
+                    "artist": {
+                        "name": "Cher",
+                        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                        "url": "https://www.last.fm/music/Cher"
+                    }
+                }
+            ]
+        },
+        "tags": {
+            "tag": [
+                {
+                    "name": "pop",
+                    "url": "https://www.last.fm/tag/pop"
+                },
+                {
+                    "name": "90s",
+                    "url": "https://www.last.fm/tag/90s"
+                },
+                {
+                    "name": "dance",
+                    "url": "https://www.last.fm/tag/dance"
+                },
+                {
+                    "name": "cher",
+                    "url": "https://www.last.fm/tag/cher"
+                },
+                {
+                    "name": "albums I own",
+                    "url": "https://www.last.fm/tag/albums+I+own"
+                }
+            ]
+        },
+        "wiki": {
+            "published": "27 Jul 2008, 15:55",
+            "summary": "Believe is the twenty-third studio album by American  singer-actress Cher, released on November 10, 1998 by Warner Bros. Records. The RIAA certified it Quadruple Platinum on December 23, 1999, recognizing four million shipments in the United States; Worldwide, the album has sold more than 20 million copies, making it the biggest-selling album of her career. In 1999 the album received three Grammy Awards nominations including \"Record of the Year\", \"Best Pop Album\" and winning \"Best Dance Recording\" for the single \"Believe\". <a href=\"http://www.last.fm/music/Cher/Believe\">Read more on Last.fm</a>.",
+            "content": "Believe is the twenty-third studio album by American  singer-actress Cher, released on November 10, 1998 by Warner Bros. Records. The RIAA certified it Quadruple Platinum on December 23, 1999, recognizing four million shipments in the United States; Worldwide, the album has sold more than 20 million copies, making it the biggest-selling album of her career. In 1999 the album received three Grammy Awards nominations including \"Record of the Year\", \"Best Pop Album\" and winning \"Best Dance Recording\" for the single \"Believe\".\n\nIt was released by Warner Bros. Records at the end of 1998. The album was executive produced by Rob Dickens. Upon its debut, critical reception was generally positive. Believe became Cher's most commercially-successful release, reached number one and Top 10 all over the world. In the United States, the album was released on November 10, 1998, and reached number four on the Billboard 200 chart, where it was certified four times platinum.\n\nThe album featured a change in Cher's music; in addition, Believe presented a vocally stronger Cher and a massive use of vocoder and Auto-Tune. In 1999, the album received 3 Grammy Awards nominations for \"Record of the Year\", \"Best Pop Album\" and winning \"Best Dance Recording\". Throughout 1999 and into 2000 Cher was nominated and winning many awards for the album including a Billboard Music Award for \"Female Vocalist of the Year\", Lifelong Contribution Awards and a Star on the Walk of Fame shared with former Sonny Bono. The boost in Cher's popularity led to a very successful Do You Believe? Tour.\n\nThe album was dedicated to Sonny Bono, Cher's former husband who died earlier that year from a skiing accident.\n\nCher also recorded a cover version of \"Love Is in the Air\" during early sessions for this album. Although never officially released, the song has leaked on file sharing networks.\n\nSingles\n\n\n\"Believe\"\n\"Strong Enough\"\n\"All or Nothing\"\n\"Dov'è L'Amore\" <a href=\"http://www.last.fm/music/Cher/Believe\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply."
+        }
+    }
+};
+
+
+// for (let prop in data.album)
+//     console.log(prop);
+
+// set up Handlebars helpers and partials
+Handlebars.registerHelper('albumCover', (imgs) => imgs[2]['#text']);
+Handlebars.registerHelper('albumYear', (strDate) => new Date(strDate).getFullYear());
+Handlebars.registerHelper('trackDuration', (durationSec) => {
+    let mm = Math.floor(+durationSec / 60);
+    let ss = +durationSec % 60;
+    return mm + ':' + (ss < 10 ? '0' : '') + ss;
+});
+
+function showAlbumInfo(albumDataObj) {
+    // read data
+    const source = document.querySelector('#album-template').innerHTML.trim();
+    const template = Handlebars.compile(source);
+    const result = template(albumDataObj);
+    // display album data
+    const contentContainer = document.querySelector('.content');
+    contentContainer.style.justifyContent = 'space-between';
+    contentContainer.innerHTML = result;
+}
+
+// showAlbumInfo(data);
---
+++ js/artist.js
@@ -0,0 +1,246 @@
+'use strict';
+
+const data = {
+    "artist": {
+        "name": "Cher",
+        "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+        "url": "https://www.last.fm/music/Cher",
+        "image": [
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/34s/345a7b45ef442c8340f9b19263021c68.png",
+                "size": "small"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/64s/345a7b45ef442c8340f9b19263021c68.png",
+                "size": "medium"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/174s/345a7b45ef442c8340f9b19263021c68.png",
+                "size": "large"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/345a7b45ef442c8340f9b19263021c68.png",
+                "size": "extralarge"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/345a7b45ef442c8340f9b19263021c68.png",
+                "size": "mega"
+            },
+            {
+                "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/345a7b45ef442c8340f9b19263021c68.png",
+                "size": ""
+            }
+        ],
+        "streamable": "0",
+        "ontour": "1",
+        "stats": {
+            "listeners": "1056619",
+            "playcount": "14364905"
+        },
+        "similar": {
+            "artist": [
+                {
+                    "name": "Madonna",
+                    "url": "https://www.last.fm/music/Madonna",
+                    "image": [
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/34s/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": "small"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/64s/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": "medium"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/174s/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": "large"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": "extralarge"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": "mega"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/4d0d5f09fdb5ce83d5a2b52a834254b2.png",
+                            "size": ""
+                        }
+                    ]
+                },
+                {
+                    "name": "Kylie Minogue",
+                    "url": "https://www.last.fm/music/Kylie+Minogue",
+                    "image": [
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/34s/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": "small"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/64s/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": "medium"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/174s/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": "large"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": "extralarge"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": "mega"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/19c2e7667d3a441a9c47697014ddabc7.png",
+                            "size": ""
+                        }
+                    ]
+                },
+                {
+                    "name": "Sonny & Cher",
+                    "url": "https://www.last.fm/music/Sonny+&+Cher",
+                    "image": [
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/34s/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": "small"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/64s/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": "medium"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/174s/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": "large"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": "extralarge"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": "mega"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/3c33108af6fc4393b7a8501d9daf4bae.png",
+                            "size": ""
+                        }
+                    ]
+                },
+                {
+                    "name": "Céline Dion",
+                    "url": "https://www.last.fm/music/C%C3%A9line+Dion",
+                    "image": [
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/34s/2958acb4f43f1dface9302030c754e75.png",
+                            "size": "small"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/64s/2958acb4f43f1dface9302030c754e75.png",
+                            "size": "medium"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/174s/2958acb4f43f1dface9302030c754e75.png",
+                            "size": "large"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/2958acb4f43f1dface9302030c754e75.png",
+                            "size": "extralarge"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/2958acb4f43f1dface9302030c754e75.png",
+                            "size": "mega"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/2958acb4f43f1dface9302030c754e75.png",
+                            "size": ""
+                        }
+                    ]
+                },
+                {
+                    "name": "Cyndi Lauper",
+                    "url": "https://www.last.fm/music/Cyndi+Lauper",
+                    "image": [
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/34s/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": "small"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/64s/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": "medium"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/174s/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": "large"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": "extralarge"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": "mega"
+                        },
+                        {
+                            "#text": "https://lastfm-img2.akamaized.net/i/u/300x300/594903d3355cc5f6c4d934b52173007b.png",
+                            "size": ""
+                        }
+                    ]
+                }
+            ]
+        },
+        "tags": {
+            "tag": [
+                {
+                    "name": "pop",
+                    "url": "https://www.last.fm/tag/pop"
+                },
+                {
+                    "name": "female vocalists",
+                    "url": "https://www.last.fm/tag/female+vocalists"
+                },
+                {
+                    "name": "80s",
+                    "url": "https://www.last.fm/tag/80s"
+                },
+                {
+                    "name": "dance",
+                    "url": "https://www.last.fm/tag/dance"
+                },
+                {
+                    "name": "rock",
+                    "url": "https://www.last.fm/tag/rock"
+                }
+            ]
+        },
+        "bio": {
+            "links": {
+                "link": {
+                    "#text": "",
+                    "rel": "original",
+                    "href": "https://last.fm/music/Cher/+wiki"
+                }
+            },
+            "published": "17 Feb 2006, 22:09",
+            "summary": "Cher (born Cherilyn Sarkisian; May 20, 1946) is an Oscar- and Grammy-winning American singer and actress. A major figure for over five decades in the world of popular culture, she is often referred to as the Goddess of Pop for having first brought the sense of female autonomy and self-actualization into the entertainment industry. \n\nShe is known for her distinctive contralto and for having worked extensively across media, as well as for continuously reinventing both her music and image, the latter of which has been known to induce controversy. <a href=\"https://www.last.fm/music/Cher\">Read more on Last.fm</a>",
+            "content": "Cher (born Cherilyn Sarkisian; May 20, 1946) is an Oscar- and Grammy-winning American singer and actress. A major figure for over five decades in the world of popular culture, she is often referred to as the Goddess of Pop for having first brought the sense of female autonomy and self-actualization into the entertainment industry. \n\nShe is known for her distinctive contralto and for having worked extensively across media, as well as for continuously reinventing both her music and image, the latter of which has been known to induce controversy.\n\nCher was born Cherilyn Sarkisian in El Centro, California, on May 20, 1946. She is of Armenian-American, Irish, English, German, and Cherokee ancestry. Cher first caught the eye and ear of the public in 1965 as one-half of the pop rock duo Sonny & Cher, which popularized a peculiar smooth sound that competed successfully with the predominant British Invasion and Motown Sound of the era. After a period in which the duo became obsolete, she re-emerged in the 1970's as a television personality with her shows The Sonny & Cher Comedy Hour and Cher, which attained great popularity.\n\nAt the same time, she established herself as a solo artist with hits such as \"Bang Bang (My Baby Shot Me Down)\", \"Gypsys, Tramps & Thieves\", \"Half-Breed\", and \"Dark Lady\", which dealt with taboo subjects in mainstream popular music. Throughout, she cemented her status as a fashion trendsetter with her daring outfits, and was noted as being the first woman to expose her navel on television. Cher's impact at that time, as described by Phill Marder from Goldmine magazine, \"led the way to advance feminine rebellion in the rock world,\" as she was \"the prototype of the female rock star, setting the standard for appearance and [...] attitude[.]\"\n\nIn 1983 Cher made a critically acclaimed appearance on Broadway and starred in the film Silkwood, which earned her a nomination for the Academy Award for Best Supporting Actress. In the following years, she became an acclaimed film actress by starring in a string of hit films, including Mask, The Witches of Eastwick, and Moonstruck, for which she won the Academy Award for Best Actress in 1988. At the same time, she established herself as a \"serious rock and roller\" by releasing a series of rock albums and hit singles such as \"I Found Someone\", \"If I Could Turn Back Time\", and \"The Shoop Shoop Song (It's in His Kiss)\".\n\nShe made her directing debut with the film If These Walls Could Talk in 1996 and released the biggest-selling single of her career thus far, \"Believe\", which revolutionized the recording industry because of its pioneer use of Auto-Tune (also known as the \"Cher effect\"). Throughout the 2000's she embarked on a series of concert tours that were among the highest-grossing of all-time.\n\nCher has won an Academy Award, a Grammy Award, an Emmy Award, three Golden Globe Awards, and the Best Actress Award at the Cannes Film Festival for her work in film, music and television. She is the only person  to receive all of these awards. She is also the only artist to have notched a number-one single on a Billboard chart in each of the past six decades. Recognized as one of the best-selling music artists of all time, she has sold more than 100 million solo albums worldwide and 40 million records as part of Sonny & Cher.\n\nOfficial website: http://www.cher.com/ <a href=\"https://www.last.fm/music/Cher\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply."
+        }
+    }
+};
+
+Handlebars.registerHelper('artistGenres', (arrTags) => arrTags.reduce((acc, tag) => acc + (acc = '') ? tag.name : ' / ' + tag.name));
+
+function showArtistInfo(artistDataObj) {
+    // read data
+    const source = document.querySelector('#artist-page-template').innerHTML.trim();
+    const template = Handlebars.compile(source);
+    const result = template(artistDataObj);
+    // display artist data
+    const contentContainer = document.querySelector('.content');
+    // contentContainer.style.justifyContent = 'space-between';
+    contentContainer.innerHTML = result;
+}
+
+showArtistInfo(data);
