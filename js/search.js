cdca83a900fa12566bf13ed48b6129b7d648ea5e
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-07-21 10:58:40 +0300

    added track&album info requests

--- js/search.js
+++ js/search.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//==================================== TRACK SEARCH ===================================================================
 // (function () {
 //     let searchInput = document.querySelector('.search__input');
 //     let searchBtn = document.querySelector('.search__btn');
@@ -44,7 +46,7 @@
 //                     searchInput.value ='';
 //                     localStorage.setItem( "searchResultTracks", JSON.stringify(trackData) );
 //                 }
-//             });
+//             }).catch(error => console.error("Error: ", error));
 //     }
 //
 //     function createTrackCard(){
@@ -54,63 +56,99 @@
 //         container.innerHTML = template(trackData);
 //     }
 // })();
+//==================================================== ALBUM SEARCH ================================================
 
+// (function () {
+//     let searchInput = document.querySelector('.search__input');
+//     let searchBtn = document.querySelector('.search__btn');
+//     let albumArr =[];
+//     let albumData = {
+//         albums: []
+//     };
+//
+//     searchBtn.addEventListener('click', searchAlbum);
+//
+//     function searchAlbum(event){
+//         event.preventDefault();
+//         localStorage.removeItem("searchResultAlbums");
+//         albumData = {
+//             albums: []
+//         };
+//         albumArr =[];
+//         let searchQuery = searchInput.value;
+//         let url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${searchQuery}&api_key=e900a41307805d11c3527e8aeebf5d4b&format=json`;
+//         fetch(url)
+//             .then(response=> response.ok ? response.json():null)
+//             .then(data=> {
+//                 albumArr.push(data);
+//                 console.log(albumArr);
+//                 albumDataPreparation();
+//                 function albumDataPreparation(){
+//                     albumArr[0].results.albummatches.album.forEach(function(item){
+//                         let albumName = item.name;
+//                         let albumArtist = item.artist;
+//                         let albumImage = item.image[2]['#text'];
+//                         albumData.albums.push(
+//                             {
+//                                 albumName: albumName,
+//                                 albumArtist: albumArtist,
+//                                 albumImage: albumImage,
+//                             }
+//                         )
+//                     });
+//                     createAlbumCard();
+//                     console.log(albumData);
+//                     searchInput.value ='';
+//                     localStorage.setItem( "searchResultAlbums", JSON.stringify(albumData) );
+//                 }
+//             })
+//             .catch(error => console.error("Error: ", error));
+//     }
+//
+//     function createAlbumCard(){
+//         const source = document.querySelector('#albums-list-template').innerHTML.trim();
+//         console.log(source);
+//         const container = document.querySelector('.content');
+//         const template = Handlebars.compile(source);
+//         container.innerHTML = template(albumData);
+//     }
+// })();
 
-(function () {
-    let searchInput = document.querySelector('.search__input');
-    let searchBtn = document.querySelector('.search__btn');
-    let albumArr =[];
-    let albumData = {
-        albums: []
-    };
+//============================================ GET TRACK INFO ==========================================================
+// (function () {
+//     let TrackInfoArr =[];
+//     let artist = 'Madonna';
+//     let track = 'Frozen';
+//
+//     getTrackInfo();
+//     function getTrackInfo() {
+//         TrackInfoArr = [];
+//         let url = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=e900a41307805d11c3527e8aeebf5d4b&artist=${artist}&track=${track}&format=json`;
+//         fetch(url)
+//             .then(response => response.ok ? response.json() : null)
+//             .then(data => {
+//                 TrackInfoArr.push(data);
+//                 console.log(TrackInfoArr)
+//             })
+//             .catch(error => console.error("Error: ", error))
+//     }}) ();
 
-    searchBtn.addEventListener('click', searchAlbum);
+//==========================================GET ALBUM INFO ============================================================
 
-    function searchAlbum(event){
-        event.preventDefault();
-        localStorage.removeItem("searchResultAlbums");
-        albumData = {
-            albums: []
-        };
-        albumArr =[];
-        let searchQuery = searchInput.value;
-        let url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${searchQuery}&api_key=e900a41307805d11c3527e8aeebf5d4b&format=json`;
-        fetch(url)
-            .then(response=> response.ok ? response.json():null)
-            .then(data=> {
-                albumArr.push(data);
-                console.log(albumArr);
-                albumDataPreparation();
-                function albumDataPreparation(){
-                    albumArr[0].results.albummatches.album.forEach(function(item){
-                        let albumName = item.name;
-                        let albumArtist = item.artist;
-                        let albumImage = item.image[2]['#text'];
-                        albumData.albums.push(
-                            {
-                                albumName: albumName,
-                                albumArtist: albumArtist,
-                                albumImage: albumImage,
-                            }
-                        )
-                    });
-                    createAlbumCard();
-                    console.log(albumData);
-                    searchInput.value ='';
-                    localStorage.setItem( "searchResultAlbums", JSON.stringify(albumData) );
-                }
-            });
-    }
-
-    function createAlbumCard(){
-        const source = document.querySelector('#albums-list-template').innerHTML.trim();
-        console.log(source);
-        const container = document.querySelector('.content');
-        const template = Handlebars.compile(source);
-        container.innerHTML = template(albumData);
-    }
-})();
-
-
-
-
+// (function () {
+//     let AlbumInfoArr =[];
+//     let artist = 'Madonna';
+//     let album = 'Ray of Light';
+//
+//     getAlbumInfo();
+//     function getAlbumInfo() {
+//         AlbumInfoArr = [];
+//         let url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=e900a41307805d11c3527e8aeebf5d4b&artist=${artist}&album=${album}&format=json`;
+//         fetch(url)
+//             .then(response => response.ok ? response.json() : null)
+//             .then(data => {
+//                 AlbumInfoArr.push(data);
+//                 console.log(AlbumInfoArr)
+//             })
+//             .catch(error => console.error("Error: ", error))
+//     }}) ();
